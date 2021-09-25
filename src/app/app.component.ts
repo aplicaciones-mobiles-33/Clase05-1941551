643e11bd3ee1519272c5da784264a08cb3ceae30
Emiliano Sanchez <foreveremiliano9@gmail.com>
2021-09-25 13:16:25 -0500

    Clase 05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  usuarios = ['EmilianoSanchez', 'ruffles', 'usuario123'];
+
+  avatar= '../assets/imagenes/avatar.jpg'
 }
